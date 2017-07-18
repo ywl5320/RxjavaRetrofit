95002115f8d132fc16d482c38d9c54712e37996b
596353036@qq.com <ywl12345>
2017-07-18 11:10:35 +0800

    升级到OKhttp2+Rxjava2+Retrofit2,整体结构没变

---
+++ RxJava2Retrofit2Okhttp3/RxJavaRetrofit/app/src/main/java/com/ywl5320/rxjavaretrofit/httpservice/serviceapi/UserApi.java
@@ -0,0 +1,42 @@
+package com.ywl5320.rxjavaretrofit.httpservice.serviceapi;
+
+
+import com.ywl5320.rxjavaretrofit.httpservice.beans.UserBean;
+import com.ywl5320.rxjavaretrofit.httpservice.beans.WeatherBean;
+import com.ywl5320.rxjavaretrofit.httpservice.service.BaseApi;
+import com.ywl5320.rxjavaretrofit.httpservice.service.HttpMethod;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+
+/**
+ * Created by ywl on 2016/5/19.
+ */
+public class UserApi extends BaseApi {
+
+    public static UserApi userApi;
+    public UserService userService;
+    public UserApi()
+    {
+        userService = HttpMethod.getInstance().createApi(UserService.class);
+    }
+
+    public static UserApi getInstance()
+    {
+        if(userApi == null)
+        {
+            userApi = new UserApi();
+        }
+        return userApi;
+    }
+    /*-------------------------------------获取的方法-------------------------------------*/
+
+    public void getWeather(String loaction, String output, String ak, Observer<WeatherBean> subscriber)
+    {
+        Observable observable = userService.getWeather(loaction, output, ak)
+                .map(new HttpResultFunc<WeatherBean>());
+
+        toSubscribe(observable, subscriber);
+    }
+
+}
